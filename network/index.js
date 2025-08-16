24618618ccff134c64aa103477fe07489dca4162
isqualham <williamfacme@gmail.com>
2025-08-15 21:35:09 -0300

    a

--- network/index.js
+++ network/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-//db e o nome da imagem que esta o banco de dados
+//db e o nome do container m que esta o banco de dados
 mongoose.connect("mongodb://db:27017/test",{})
 .then(() => console.log("Connected to MongoDB"))
 .catch((err)=> console.log(err));
