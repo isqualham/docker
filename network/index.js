686f4b2f4f7c68ff55117000d9e72421ccf3f94f
isqualham <williamfacme@gmail.com>
2025-08-15 21:27:17 -0300

    network

---
+++ network/index.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+//db e o nome da imagem que esta o banco de dados
+mongoose.connect("mongodb://db:27017/test",{})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err)=> console.log(err));
